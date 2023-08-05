700b1d7be3d0c6a9abd2552cec6f7da254dced8e
Brian <lcy@BriandeMacBook-Pro.local>
2023-08-06 03:53:43 +0800

    First edition to autometly scrape pictures

--- Services/ScrapeGettyimages.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-import time, os, requests
